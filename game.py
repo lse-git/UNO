f4ac084ea3051d1dae1773800eddd9b7333b9eac
lse-git <luis.schmidteisenlohr@googlemail.com>
2021-01-16 10:51:20 +0100

    removed for loop in game.py

--- game.py
+++ game.py
@@ -4,6 +4,3 @@ if __name__ == "__main__":
     game = Uno()
     while True:
         for player in game.players:
-            game.give_player_card(player)
-            print(player.deck)
-            print(len(player.deck))
